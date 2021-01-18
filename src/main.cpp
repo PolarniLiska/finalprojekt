ab34c254e9da0bffd8048a086f59c5b81b620ced
Jan Spiška <45061669+PolarniLiska@users.noreply.github.com>
2021-01-18 12:41:13 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,31 @@
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <Wire.h>
-#include "index.h"
+#include "index.html"
  
 const char* ssid = "Wifi Honza";
 const char* password = "honzik2001";
  
 ESP8266WebServer server(80);
- 
+
 void handleRoot() {
-String s = webstranka;
-server.send(200, "text/html", s);
+String web = webstranka;
+server.send(200, "text/html", web);
 }
  
 void handleADC() {
 int rain = analogRead(A0);
+int raind = analogRead(D1);
 String adc = String(rain);
+String adc1 = String(raind);
 
-String data = "{\"Rain\":\""+String(rain)+"\"}";
+String data = "{\"Rain\":\""+String(rain)+"\",\"Pressuremb\":\""+String(raind)+"\"}";
 server.send(200,"text/plane",data);
 
+Serial.print("D:");
+Serial.println(adc1);
 Serial.print("R:");
 Serial.println(adc);
 }
@@ -44,6 +49,7 @@ Serial.println(WiFi.localIP());
 
 server.on("/", handleRoot);
 server.on("/readADC", handleADC);
+server.on("/", setup);
 server.begin();
 Serial.println("Server se spustil");
 }
